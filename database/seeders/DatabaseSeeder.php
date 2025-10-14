00c3672b1921da802eb0a352a9723a052b856141
NovaRaii <pkoppany40@gmail.com>
2025-10-14 12:15:07 +0200

    Migrations, Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,13 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
+{
+    $this->call([
+        AuthorSeeder::class,
+        CategorySeeder::class,
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    $this->call(BookSeeder::class); // mindig utolsó
+}
+
 }
